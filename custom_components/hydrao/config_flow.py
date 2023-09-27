7141d683f444abea675adf2a3085a40c61874902
sebcc <sebastien.carecolin@gmail.com>
2023-09-27 17:17:38 +0000

    handle unique config

--- custom_components/hydrao/config_flow.py
+++ custom_components/hydrao/config_flow.py
@@ -1,7 +1,13 @@
 import logging
 import voluptuous as vol
+from typing import Any, Dict, Optional
 import homeassistant.helpers.config_validation as cv
 from homeassistant.helpers.aiohttp_client import async_get_clientsession
+from homeassistant.helpers.entity_registry import (
+    async_entries_for_config_entry,
+    async_get,
+
+)
 from homeassistant import config_entries
 from homeassistant.core import  callback
 from homeassistant.const import CONF_EMAIL, CONF_PASSWORD, CONF_API_KEY,CONF_DEVICES
@@ -55,7 +61,10 @@ class ConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
         """Handle the initial step."""
         errors = {}
         _LOGGER.debug("In async_step_user !!")
+
         if user_input is not None:
+            await self.async_set_unique_id(user_input[CONF_EMAIL].lower())
+            self._abort_if_unique_id_configured()
             session = async_get_clientsession(self.hass)
             self._client= HydraoAPI(config=user_input,session=session)
             try:
@@ -83,23 +92,68 @@ class ConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
                     {vol.Required(CONF_DEVICES):cv.multi_select(device_list)}
                 )
                 return self._show_setup_form("devices", user_input, data_schema, errors)
+
         self.data[CONF_DEVICES]=user_input[CONF_DEVICES]
         return self.async_create_entry(title=TITLE,
                                        data=self.data,
                                        options={CONF_DEVICES: user_input[CONF_DEVICES]})
-    # @staticmethod
-    # @callback
-    # def async_get_options_flow(config_entry):
-    #     """Get the options flow for this handler."""
-    #     return HydraoOptionsFlowHandler(config_entry)
+    @staticmethod
+    @callback
+    def async_get_options_flow(config_entry):
+        """Get the options flow for this handler."""
+        return HydraoOptionsFlowHandler(config_entry)
 
-# class HydraoOptionsFlowHandler(config_entries.OptionsFlow):
-#     """Handles options flow for the component."""
+class HydraoOptionsFlowHandler(config_entries.OptionsFlow):
+    """Handles options flow for the component."""
 
-#     def __init__(self, config_entry: config_entries.ConfigEntry) -> None:
-#         self.config_entry = config_entry
+    def __init__(self, config_entry: config_entries.ConfigEntry) -> None:
+        self.config_entry = config_entry
 
-#     async def async_step_init(
-#             self, user_input: Dict[str, Any] = None
-#         ) -> Dict[str, Any]:
-#         pass
+    async def async_step_init(
+            self, user_input: Dict[str, Any] = None
+        ) -> Dict[str, Any]:
+        """Manage the options for the custom component."""
+        errors: Dict[str, str] = {}
+        entity_registry = await async_get_registry(self.hass)
+        entries = async_entries_for_config_entry(
+            entity_registry, self.config_entry.entry_id
+        )
+        all_devices = {e.entity_id: e.original_name for e in entries}
+        device_map = {e.entity_id: e for e in entries}
+        if user_input is not None:
+            updated_devices = deepcopy(self.config_entry.data[CONF_DEVICES])
+
+            # Remove any unchecked repos.
+            removed_entities = [
+                entity_id
+                for entity_id in device_map.keys()
+                if entity_id not in user_input["devices"]
+            ]
+            for entity_id in removed_entities:
+                entity_registry.async_remove(entity_id)
+                # Remove from our configured repos.
+                entry = device_map[entity_id]
+                entry_path = entry.unique_id
+                updated_device = [e for e in updated_device if e["devices"] != entry_path]
+            if not errors :
+                return self.async_create_entry(
+                    title="",
+                    data={CONF_DEVICES: updated_devices},
+                )
+        try:
+            devices = await get_devices(self._client, user_input) # get device list
+            device_list={}
+            device_list= { device['device_uuid']: device['label'] for device in devices}
+        except ValueError:
+            errors["base"] = "devices"
+        #TODO compare device list from api & from entity registry to fill all_device and check the one already existing in HA
+        options_schema = vol.Schema(
+            {
+                vol.Optional("devices", default=list(all_devices.keys())): cv.multi_select(
+                    all_devices
+                ),
+            }
+        )
+        return self.async_show_form(
+            step_id="init", data_schema=options_schema, errors=errors
+        )
